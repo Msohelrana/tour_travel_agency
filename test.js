c0b252f2e9a79772a21626985a01a1948a24f0c4
Msohelrana <mdsohelrana5860@gmsil.com>
2022-08-28 15:27:51 +0600

    first

---
+++ test.js
@@ -0,0 +1,11 @@
+var heading3=document.createElement("h1");
+var text=document.createTextNode("This is heading 3");
+heading3.appendChild(text);
+var myDiv=document.getElementById("my-div");
+myDiv.appendChild(heading3);
+var heading1=document.getElementsByTagName("h1")[0];
+var heading0=document.createElement("h1");
+var text0=document.createTextNode("This is heading 0");
+heading0.appendChild(text0);
+myDiv.insertBefore(heading0,heading1);
+myDiv.removeChild(heading1);
